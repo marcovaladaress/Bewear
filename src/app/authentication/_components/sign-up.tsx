4363f0954ed4efdf7a31d9cc05d5f9e7e844e087
marco valadares <marcovf99@gmail.com>
2025-08-06 19:46:25 -0300

    fix: add sign up

--- src/app/authentication/_components/sign-up.tsx
+++ src/app/authentication/_components/sign-up.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -22,6 +24,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z
   .object({
@@ -44,11 +47,8 @@ const formSchema = z
 
 type FormValues = z.infer<typeof formSchema>;
 
-function onSubmit(values: FormValues) {
-  console.log(values);
-}
-
 const SignUpForm = () => {
+  const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -58,6 +58,29 @@ const SignUpForm = () => {
       passwordConfirmation: "",
     },
   });
+
+  async function onSubmit(values: FormValues) {
+    await authClient.signUp.email({
+      name: values.name,
+      email: values.email,
+      password: values.password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: (error) => {
+          if (error.error.code === "USER_ALREADY_EXISTS") {
+            toast.error("E-mail já cadastrado");
+             form.setError("email", {
+               message: "E-mail já cadastrado",
+             });
+          }
+          toast.error(error.error.message);
+        },
+      },
+    });
+  }
+
   return (
     <>
       <Card>
